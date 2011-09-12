72a7fd75fca35adc9821cb0b6bc6f82ff26db1c7
Ilya Kuznetsov <worklez@gmail.com>
2011-09-12 20:32:28 +0700

    [bugzid #] Added title helper

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -1,6 +1,7 @@
 class DebugController < ApplicationController
   def view_company
     @company = CompanyProfile.find(params[:id])
+    @title = @company.name
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def page_title
+    ((@title + " &mdash; " if @title).to_s + 'Nanotaiga').html_safe
+  end
 end
